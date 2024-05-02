cb82c299de5f011f1cf1b039a68c019dfd4d14bb
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-05-02 16:55:01 +0300

    homework 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesDisplay = Markup(galleryItems);
+
+function Markup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original.value}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesDisplay);
+
+const clickContainer = (element) => {
+  element.preventDefault();
+  if (element.target.classList.contains("gallery")) return;
+  const target = element.target.dataset.source;
+
+  const instanceBox = basicLightbox.create(`
+    <img src="${target}"width="800" height="600">`);
+
+  instanceBox.show();
+};
+
+galleryContainer.addEventListener("click", clickContainer);
